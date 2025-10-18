1074088dc10dec0ca829dbb12afaa667f9be5091
Dzyundzya Alexander <dzyundzya.aa@yandex.ru>
2025-10-18 17:12:20 +0300

    feat: add admin for users

---
+++ server/apps/users/admin.py
@@ -0,0 +1,51 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from server.apps.users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    """Admin interface for CustomUser."""
+
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'role',
+        'is_active',
+        'is_staff',
+        'is_superuser',
+    )
+
+    list_filter = (
+        'is_active',
+        'role',
+    )
+    search_fields = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
+    readonly_fields = ('last_login', 'date_joined')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {
+            'fields': (
+                'role',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            ),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    ordering = ('id',)
